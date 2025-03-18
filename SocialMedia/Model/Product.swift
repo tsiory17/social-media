fa69b2cfc3822c6a5317f82f7e019f7e52899f46
Thiago <thiagophneto@gmail.com>
2025-03-18 09:02:10 -0400

    product model created

---
+++ SocialMedia/Model/Product.swift
@@ -0,0 +1,15 @@
+//
+//  Product.swift
+//  SocialMedia
+//
+//  Created by Thiago Padilha on 2025-03-18.
+//
+
+import Foundation
+
+struct Product: Codable {
+    let id: String
+    let prodName: String
+    let Description: String
+    let price: Float
+}
