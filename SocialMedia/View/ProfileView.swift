6514c12d93607955114b6c3b138b94f4091dfc60
Thiago <thiagophneto@gmail.com>
2025-03-20 14:19:16 -0400

    Tabbar with Profile and Setting raw views added

---
+++ SocialMedia/View/ProfileView.swift
@@ -0,0 +1,18 @@
+//
+//  ProfileView.swift
+//  SocialMedia
+//
+//  Created by Thiago Padilha on 2025-03-20.
+//
+
+import SwiftUI
+
+struct ProfileView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ProfileView()
+}
---
+++ SocialMedia/View/SettingsView.swift
@@ -0,0 +1,18 @@
+//
+//  SettingsView.swift
+//  SocialMedia
+//
+//  Created by Thiago Padilha on 2025-03-20.
+//
+
+import SwiftUI
+
+struct SettingsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SettingsView()
+}
